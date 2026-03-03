26c099611120ad5fd81a42fef438042c16ea00ef
Kirtan Kumar Kori <your_github_email@example.com>
2026-03-04 00:13:23 +0530

    Implement user registration logic with validation, file uploads, and error handling

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,82 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-export const registerUser = asyncHandler(async (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     console.log("Request reached controller");
+    //get user details from forntend
+    const {fullName, email ,username, password}=req.body
+    console.log("email:", email);
 
-    res.status(200).json({
-        message: "Kirtan ka Backend Environment"
-    });
+    //validation-Not Empty
+
+    if ([fullName,email,username.password].some((field) => {
+        field?.trim() === ""
+
+    })){
+        throw new ApiError(400,"All Fileds are required and should not be empty is Required")
+       }
+
+    //check if user already exists: username and email
+
+    const existedUser = User.findOne({
+        $or:[{username},{email}]
+    })
+
+    if (existedUser) {
+        throw new ApiError(409,"User Exists Already")
+    }
+
+    //files-avtar and cover image!check
+    const avtarlocalPath = req.files?.avtar[0]?.path;
+    const coverImagePath = req.files?.coverImage[0]?.path;
+
+    if (avtarlocalPath) {
+        throw new ApiError(400,"Avtar File is Required!")
+    }
+    //upload them to cloudnary-reference url //avtar check
+    const avtar = await uploadOnCloudinary(avtarlocalPath)
+    const coverImage = await uploadOnCloudinary(coverImagePath)
+
+    if (!avtar) {
+        throw ApiError(400,"Avtar file is Required!")
+    }
+
+    
+    //create user object -- create entry in db
+    await User.create({
+        fullName,
+        avtar:avtar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
+    })
+
+    //remove password and refresh token field from response
+    const createdUser = await User.findById(User._id).select(
+        "-password -refreshToken"
+    )
+
+    
+    //check for user creation
+    if (createdUser) {
+        throw new ApiError(500,"Something went wrong while Registering the user")
+    }
+
+    
+    //return response
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser,"User Registerd Succesfully")
+    )
+
+
+
 });
+
+export {registerUser,}
+
+//http://localhost:8000/api/v1/users/register
